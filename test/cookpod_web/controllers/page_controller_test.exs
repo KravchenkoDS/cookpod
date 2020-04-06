1cf918b87358d91608e537fd6621b43f14bc7ed3
KDS <ds.krav@gmail.com>
2020-04-06 19:58:46 +0800

    Add page Terms and etc

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule CookpodWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    #assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ CookpodWeb.Gettext.gettext("Welcome to Phoenix!")    
   end
 end
