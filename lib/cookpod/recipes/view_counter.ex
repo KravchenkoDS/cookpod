0bf8b1f3b60d03621ab1003fff3f8d6104c93c85
KDS <ds.krav@gmail.com>
2020-06-11 10:41:51 +0800

    Lesson16

--- lib/cookpod/recipes/view_counter.ex
+++ lib/cookpod/recipes/view_counter.ex
@@ -2,23 +2,50 @@ defmodule Cookpod.Recipes.ViewCounter do
   use GenServer
 
   @moduledoc false
+  @table :recipe_views
+  @end_of_table :"$end_of_table"  
+  @batch_size 100
 
   def start_link(_opts) do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
-  def inc(recipe_id), do: GenServer.cast(__MODULE__, {:increment, recipe_id})
+  def init(state) do
+    :ets.new(@table, [:named_table, :set, :protected, read_concurrency: true])
+    {:ok, state}
+  end
 
-  def stats(), do: GenServer.call(__MODULE__, :stats)
-
-  def init(state), do: {:ok, state}
-
-  def handle_cast({:increment, recipe_id}, state) do
-    recipe_stats = Map.get(state, recipe_id, %{id: recipe_id, views: 0})
-    views_count = Map.fetch!(recipe_stats, :views)
-    new_state = Map.put(state, recipe_id, %{recipe_stats | views: views_count + 1})
+  def handle_cast({:increment, id}, _state) do
+    new_state = Map.update!(get_stats(id), :views, &(&1 + 1))
+    :ets.insert(@table, {id, new_state})
     {:noreply, new_state}
   end
 
-  def handle_call(:stats, _from, state), do: {:reply, Map.values(state), state}
+  def handle_call(:stats, _from, state), do: {:reply, Map.values(state), state}  
+
+  def inc(id), do: GenServer.cast(__MODULE__, {:increment, id})
+
+  def stats() do
+    Stream.resource(
+      fn ->
+            :ets.select(@table, [{{:"$1", :"$2"}, [], [:"$2"]}], @batch_size)
+      end,      
+      fn acc ->
+            case acc do
+              @end_of_table ->
+                {:halt, acc}
+              {list, continuation} ->
+                {list, :ets.select(continuation)}
+            end
+      end,      
+      fn _ -> :ok end
+    )
+  end
+
+  def get_stats(id) do
+    case :ets.lookup(@table, id) do
+      [] -> %{id: id, views: 0}
+      [{_key, stats}] -> stats
+    end
+  end
 end
